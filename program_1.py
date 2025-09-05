0bcb5737e386e28d6c3bdd5519346ff2cf348feb
Arianna0209 <agendres@students.unwsp.edu>
2025-09-05 18:20:04 -0400

    Modified Hello World Program Assignment

--- program_1.py
+++ program_1.py
@@ -1,6 +1,10 @@
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+
+    #Author: Arianna Endres
+    #Date: 09/05/2025
+    #Title: Hello World
+    print('Hello World')
 
 hello_world()
--- program_2.py
+++ program_2.py
@@ -1,6 +1,14 @@
 def proverbs_22_29():
     # Write your Proverbs 22:29 code below
-    print('...')
+
+    #Author: Arianna Endres
+    #Date: 09/05/2025
+    #Title: Proverbs 22:29
+
+    print('“Do you see a man skillful in his work?')
+    print('He will stand before kings;')
+    print('he will not stand before obscure men.”')
+    print('     (Proverbs 22:29)')
     
 
 proverbs_22_29()
